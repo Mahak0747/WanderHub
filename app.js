8fb3f829d48848b25fbe4e3251deffadf5573246
Mahak0747 <mahak.goswami2050@gmail.com>
2025-10-23 23:00:24 +0530

    doing project-phase1 part b

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const mongoose=require("mongoose");
 const path=require("path");
 const Listing=require("./models/listing.js");
 const methodOverrid= require('method-override');
+const ejsMate=require('ejs-mate');
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverrid("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
 
 main().catch(err => console.log(err));
 async function main() {
