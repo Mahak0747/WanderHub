945e88ac1a1baade6dec2c44d6f601e681e4496f
Mahak0747 <mahak.goswami2050@gmail.com>
2026-03-29 16:08:16 +0530

    Project-Phase3 part c DONE

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ sessionOptions={
   }
 }
 
-app.get("/",(req,res)=>{
-    res.send("Hi I am root");
-});
+// app.get("/",(req,res)=>{
+//     res.send("Hi I am root");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
